b4393fa2fdd6133425e78d005c363b6581a3c0da
maxine-janka <maxinejanka@gmail.com>
2024-12-13 02:12:21 +0000

    test fixes

--- shell.c
+++ shell.c
@@ -13,13 +13,17 @@ int main(void)
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
+		{
 			write(STDOUT_FILENO, "$ ", 2);
+		}
 		line = read_line();
 		if (line == NULL)
 		{
 			if (isatty(STDIN_FILENO))
+			{
 				write(STDOUT_FILENO, "\n", 1);
 			break;
+			}
 		}
 
 		if (_strlen(line) == 0 || white_space(line))
